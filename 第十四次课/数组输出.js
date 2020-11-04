b9cd94dec45fe30bfa458bb41d19dd37ef06b817
shuying Guo <70828681+XiaoBai918@users.noreply.github.com>
2020-11-04 23:34:04 +0800

    14

---
+++ 第十四次课/数组输出.js
@@ -0,0 +1,10 @@
+// 脚本语言，用前端检查错误
+function orange(){
+    var array = [1,2,6,0,6,3,7,54,32]
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log(element);
+        
+    }
+}
+orange();
