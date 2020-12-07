bd2a403d4b24a7c84cfa99c038ecb7a3abe95bf4
shuying Guo <70828681+XiaoBai918@users.noreply.github.com>
2020-12-07 20:53:29 +0800

    js

--- 第十九次课/tempCodeRunnerFile.js
+++ 第十九次课/tempCodeRunnerFile.js
@@ -1,4 +1,12 @@
-function pear2(name){
-    return"this is"+`${name}!`
+function Pear(name,price,origin,weight){
+    this.name=name,
+    this.price=price,
+    this.origin=origin,
+    this.weighweoght;
+    this.printName=function(){
+        console.log("this is pear" + this.name)
+    }
 }
-console.log(pear2("桃子"))
+Pear.prototype.printName2=function(){
+    console.log("printName2")
+}
