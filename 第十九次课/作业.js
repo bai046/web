6cff606606efaafdb26868b715a86d6db9646ce7
shuying Guo <70828681+XiaoBai918@users.noreply.github.com>
2020-12-14 23:04:35 +0800

    js

--- 第十九次课/作业.js
+++ 第十九次课/作业.js
@@ -59,40 +59,37 @@ p.printRegions();
 
 
 
-function city(cityName,cityArea,cityPostcode,cityRegions){
-    this.cityName=cityName,
-    this.cityArea=cityArea,
-    this.cityPostcode=cityPostcode,
-    this.cityRegions=cityRegions
+// function city(cityName,cityArea,cityPostcode,cityRegions){
+//     this.cityName=cityName,
+//     this.cityArea=cityArea,
+//     this.cityPostcode=cityPostcode,
+//     this.cityRegions=cityRegions
 
-    this.cityRegions=new Array();
+//     this.cityRegions=new Array();
 		
-    city.prototype.printName=function (){
-        console.log(this.cityName)
-    }
-    city.prototype.printArea=function (){
-        console.log(this.cityArea)
-    }
-    city.prototype.printPostcode=function (){
-        console.log(this.cityPostcode)
-    }
+//     city.prototype.printName=function (){
+//         console.log(this.cityName)
+//     }
+//     city.prototype.printArea=function (){
+//         console.log(this.cityArea)
+//     }
+//     city.prototype.printPostcode=function (){
+//         console.log(this.cityPostcode)
+//     }
 
-  city.prototype.printRegions=function (){
-    // for (arr of cityRegions){
-    //     console.log(arr)        
-    // }
-    for(var i=0;i<cityRegions.length;i++){
-        console.log(cityRegions[i]+' ');
-       }
-}
+//   city.prototype.printRegions=function (){
+//     // for (arr of cityRegions){
+//     //     console.log(arr)        
+//     // }
+//     for(var i=0;i<cityRegions.length;i++){
+//         console.log(cityRegions[i]+' ');
+//        }
+// }
     
-}
-var p=new city("南昌市",888888,'abcdef',["东湖区",'西湖区',"青山湖区","新建区","湾里区"]); 
-p.printName();
-p.printArea();
-p.printPostcode();
-p.printRegions();
+// }
+// var p=new city("南昌市",888888,'abcdef',["东湖区",'西湖区',"青山湖区","新建区","湾里区"]); 
+// p.printName();
+// p.printArea();
+// p.printPostcode();
+// p.printRegions();
 
-
-// https://blog.csdn.net/plannothing/article/details/107570530
-// JavaScript中数组的字面量
