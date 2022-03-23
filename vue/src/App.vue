42d044e9f75e328d9925687d6b1fcecfbf4365c1
郭淑瑛 <2235917772@qq.com>
2022-03-23 12:04:25 +0800

    0

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
-    <!-- <My/>*** -->
+    <!-- <My/> -->
     <N/>
   </div>
 </template>
--- vue/src/components/My.vue
+++ vue/src/components/My.vue
@@ -14,7 +14,7 @@ export default {
   data(){
     return{
       lists:[1,2,3,4,5,6],
-      isShow:false
+      isShow:true
     }
   }
 }
